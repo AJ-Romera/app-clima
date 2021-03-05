5384b8fe5e485fe918dcb98b226d208801c30260
AJ-Romera <ajromeraalfonso@gmail.com>
2021-03-05 15:55:05 +0100

    CHNG: Añade más Opciones al Select de Paises

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -11,6 +11,14 @@ function Formulario() {
             <div className='input-field col s12'>
                 <select name='pais' id='pais'>
                     <option value=''>-- Seleccione un país --</option>
+                    {/* ORDENAR POR ORDEN */}
+                    <option value='AR'>Argentina</option>
+                    <option value='CO'>Colombia</option>
+                    <option value='CR'>Costa Rica</option>
+                    <option value='ES'>España</option>
+                    <option value='US'>Estados Unidos</option>
+                    <option value='MX'>México</option>
+                    <option value='PE'>Perú</option>
                 </select>
                 <label htmlFor='pais'>País: </label>
             </div>
