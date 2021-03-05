10a18d73edc7d312fb87cc778a6261c24e006fc8
AJ-Romera <ajromeraalfonso@gmail.com>
2021-03-05 16:17:18 +0100

    CHNG: Crea State de Busqueda y lo Actualiza con la Info del Formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Formulario() {
+    // State del formulario
+    const [busqueda, setBusqueda] = useState({
+        ciudad: '',
+        pais: '',
+    });
+
+    // Destructuring; obtener ciudad y país
+    const { ciudad, pais } = busqueda;
+
+    // Función que coloca los elementos en el state
+    const handleBusqueda = (e) => {
+        // Actualizar el State
+        setBusqueda({
+            ...busqueda,
+            [e.target.name]: e.target.value,
+        });
+    };
+
     return (
         <form>
             <div className='input-field col s12'>
-                <input type='text' name='ciudad' is='ciudad' />
+                <input
+                    type='text'
+                    name='ciudad'
+                    id='ciudad'
+                    value={ciudad}
+                    onChange={handleBusqueda}
+                />
                 <label htmlFor='ciudad'>Ciudad: </label>
             </div>
 
             <div className='input-field col s12'>
-                <select name='pais' id='pais'>
+                <select
+                    name='pais'
+                    id='pais'
+                    value={pais}
+                    onChange={handleBusqueda}
+                >
                     <option value=''>-- Seleccione un país --</option>
-                    {/* ORDENAR POR ORDEN */}
                     <option value='AR'>Argentina</option>
                     <option value='CO'>Colombia</option>
                     <option value='CR'>Costa Rica</option>
